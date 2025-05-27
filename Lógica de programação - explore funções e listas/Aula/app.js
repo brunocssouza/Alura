38306ff6cf4b8c14ff4f44c9b5e9fd500896dc1e
Bruno Cesar <brunoc.souza@outlook.com.br>
2025-05-27 09:42:33 -0300

    Changes made

--- Lógica de programação - explore funções e listas/Aula/app.js
+++ Lógica de programação - explore funções e listas/Aula/app.js
@@ -1,1 +1,33 @@
-let titulo = Document.querySelector('h1');
+function editarMensagem(tag, texto) {
+    const elemento = document.querySelector(tag);
+    elemento.innerHTML = texto
+}
+
+function gerarNumeroAleatorio() {
+    return Math.floor(Math.random() * 10 + 1)    
+}
+
+function verificarChute() {
+    const chute = document.querySelector('input').value;
+
+    tentativas++
+    if (chute == numeroSecreto) {
+        editarMensagem('h1', 'Você acertou o número!')
+        editarMensagem('p', `Tentativas realizadas: ${tentativas}`)
+    } else {
+        editarMensagem('h1', 'Você errou o número!')
+        if (chute > numeroSecreto) {
+            editarMensagem('p', 'O número secreto é menor')
+        } else {
+            editarMensagem('p', 'O número secreto é maior')
+        }
+    }
+}
+
+let tentativas = 0;
+let numeroSecreto = gerarNumeroAleatorio();
+editarMensagem('h1', "Jogo do Número Secreto")
+editarMensagem('p', "Escolha um número entre 1 e 10:")
+
+
+
