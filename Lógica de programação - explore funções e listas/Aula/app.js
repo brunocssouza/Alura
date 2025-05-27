df99edea1ec8f6280f51c3e122a4ee0bc4de5b21
Bruno Cesar <brunoc.souza@outlook.com.br>
2025-05-27 09:55:28 -0300

    Changes made

--- Lógica de programação - explore funções e listas/Aula/app.js
+++ Lógica de programação - explore funções e listas/Aula/app.js
@@ -1,33 +1,40 @@
 function editarMensagem(tag, texto) {
     const elemento = document.querySelector(tag);
-    elemento.innerHTML = texto
+    elemento.innerHTML = texto;
+}
+
+function limparInput(tag) {
+    const elemento = document.querySelector(tag);
+    elemento.value = "";
 }
 
 function gerarNumeroAleatorio() {
-    return Math.floor(Math.random() * 10 + 1)    
+    return Math.floor(Math.random() * 10 + 1); 
 }
 
 function verificarChute() {
     const chute = document.querySelector('input').value;
 
-    tentativas++
+    tentativas++;
     if (chute == numeroSecreto) {
-        editarMensagem('h1', 'Você acertou o número!')
-        editarMensagem('p', `Tentativas realizadas: ${tentativas}`)
+        editarMensagem('h1', 'Você acertou o número!');
+        editarMensagem('p', `Tentativas realizadas: ${tentativas}`);
+        document.getElementById('reiniciar').removeAttribute('disabled')
     } else {
-        editarMensagem('h1', 'Você errou o número!')
+        editarMensagem('h1', 'Você errou o número!');
         if (chute > numeroSecreto) {
-            editarMensagem('p', 'O número secreto é menor')
+            editarMensagem('p', 'O número secreto é menor');
         } else {
-            editarMensagem('p', 'O número secreto é maior')
+            editarMensagem('p', 'O número secreto é maior');
         }
     }
+    limparInput('input');
 }
 
 let tentativas = 0;
 let numeroSecreto = gerarNumeroAleatorio();
-editarMensagem('h1', "Jogo do Número Secreto")
-editarMensagem('p', "Escolha um número entre 1 e 10:")
+editarMensagem('h1', "Jogo do Número Secreto");
+editarMensagem('p', "Escolha um número entre 1 e 10:");
 
 
 
